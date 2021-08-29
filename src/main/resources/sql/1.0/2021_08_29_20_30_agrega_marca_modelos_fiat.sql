8d0cf548e0678436e98ae1f98dc4bbf2450a37eb
RdzSebastian <rdzsebastian@gmail.com>
2021-08-29 20:33:41 -0300

    Agrega Vehiculos fiat

---
+++ src/main/resources/sql/1.0/2021_08_29_20_30_agrega_marca_modelos_fiat.sql
@@ -0,0 +1,137 @@
+INSERT INTO MODELO (nombre, anio_desde, anio_hasta)
+    VALUES ('UNO','2010','2015');
+
+INSERT INTO MODELO (nombre, anio_desde, anio_hasta)
+    VALUES ('ZASTAVA','2010','2015');
+
+INSERT INTO MODELO (nombre, anio_desde, anio_hasta)
+    VALUES ('IDEA','2010','2015');
+
+INSERT INTO MODELO (nombre, anio_desde, anio_hasta)
+    VALUES ('124','2010','2015');
+
+INSERT INTO MODELO (nombre, anio_desde, anio_hasta)
+    VALUES ('PUNTO','2010','2015');
+
+INSERT INTO MODELO (nombre, anio_desde, anio_hasta)
+    VALUES ('147','2010','2015');
+
+INSERT INTO MODELO (nombre, anio_desde, anio_hasta)
+    VALUES ('PALIO','2010','2015');
+
+INSERT INTO MODELO (nombre, anio_desde, anio_hasta)
+    VALUES ('POLSKY','2010','2015');
+
+INSERT INTO MODELO (nombre, anio_desde, anio_hasta)
+    VALUES ('PREMIO','2010','2015');
+
+INSERT INTO MODELO (nombre, anio_desde, anio_hasta)
+    VALUES ('SIENA','2010','2015');
+
+INSERT INTO MODELO (nombre, anio_desde, anio_hasta)
+    VALUES ('STILO','2010','2015');
+
+INSERT INTO MODELO (nombre, anio_desde, anio_hasta)
+    VALUES ('TIPO','2010','2015');
+
+INSERT INTO MODELO (nombre, anio_desde, anio_hasta)
+    VALUES ('TOPOLINO','2010','2015');
+
+INSERT INTO MODELO (nombre, anio_desde, anio_hasta)
+    VALUES ('500','2010','2015');
+
+INSERT INTO MODELO (nombre, anio_desde, anio_hasta)
+    VALUES ('NUEVO','2010','2015');
+
+INSERT INTO MODELO (nombre, anio_desde, anio_hasta)
+    VALUES ('ARGO','2010','2015');
+
+INSERT INTO MODELO (nombre, anio_desde, anio_hasta)
+    VALUES ('CRONOS','2010','2015');
+
+INSERT INTO MODELO (nombre, anio_desde, anio_hasta)
+    VALUES ('ADVENTURE','2010','2015');
+
+INSERT INTO MODELO (nombre, anio_desde, anio_hasta)
+    VALUES ('500','2010','2015');
+
+INSERT INTO MODELO (nombre, anio_desde, anio_hasta)
+    VALUES ('X','2010','2015');
+
+INSERT INTO MODELO (nombre, anio_desde, anio_hasta)
+    VALUES ('FIORINO','2010','2015');
+
+INSERT INTO MODELO (nombre, anio_desde, anio_hasta)
+    VALUES ('FURGON','2010','2015');
+
+INSERT INTO MODELO (nombre, anio_desde, anio_hasta)
+    VALUES ('STRADA','2010','2015');
+    
+INSERT INTO VEHICULO (marca, modelo_id, tipo)
+    VALUES ('FIAT','1','AUTO');
+    
+INSERT INTO VEHICULO (marca, modelo_id, tipo)
+    VALUES ('FIAT','2','AUTO');
+    
+INSERT INTO VEHICULO (marca, modelo_id, tipo)
+    VALUES ('FIAT','3','AUTO');
+    
+INSERT INTO VEHICULO (marca, modelo_id, tipo)
+    VALUES ('FIAT','4','AUTO');
+    
+INSERT INTO VEHICULO (marca, modelo_id, tipo)
+    VALUES ('FIAT','5','AUTO');
+    
+INSERT INTO VEHICULO (marca, modelo_id, tipo)
+    VALUES ('FIAT','6','AUTO');
+   
+INSERT INTO VEHICULO (marca, modelo_id, tipo)
+    VALUES ('FIAT','7','AUTO');
+
+INSERT INTO VEHICULO (marca, modelo_id, tipo)
+    VALUES ('FIAT','8','AUTO');
+
+INSERT INTO VEHICULO (marca, modelo_id, tipo)
+    VALUES ('FIAT','9','AUTO');
+    
+INSERT INTO VEHICULO (marca, modelo_id, tipo)
+    VALUES ('FIAT','10','AUTO');
+  
+INSERT INTO VEHICULO (marca, modelo_id, tipo)
+    VALUES ('FIAT','11','AUTO');
+    
+INSERT INTO VEHICULO (marca, modelo_id, tipo)
+    VALUES ('FIAT','12','AUTO');
+  
+INSERT INTO VEHICULO (marca, modelo_id, tipo)
+    VALUES ('FIAT','13','AUTO');
+    
+INSERT INTO VEHICULO (marca, modelo_id, tipo)
+    VALUES ('FIAT','14','AUTO');
+
+INSERT INTO VEHICULO (marca, modelo_id, tipo)
+    VALUES ('FIAT','15','AUTO');
+    
+INSERT INTO VEHICULO (marca, modelo_id, tipo)
+    VALUES ('FIAT','16','AUTO');
+
+INSERT INTO VEHICULO (marca, modelo_id, tipo)
+    VALUES ('FIAT','17','AUTO');
+
+INSERT INTO VEHICULO (marca, modelo_id, tipo)
+    VALUES ('FIAT','18','AUTO');
+       
+INSERT INTO VEHICULO (marca, modelo_id, tipo)
+    VALUES ('FIAT','19','AUTO');
+    
+INSERT INTO VEHICULO (marca, modelo_id, tipo)
+    VALUES ('FIAT','20','AUTO');
+
+INSERT INTO VEHICULO (marca, modelo_id, tipo)
+    VALUES ('FIAT','21','AUTO');
+    
+INSERT INTO VEHICULO (marca, modelo_id, tipo)
+    VALUES ('FIAT','22','AUTO');
+       
+INSERT INTO VEHICULO (marca, modelo_id, tipo)
+    VALUES ('FIAT','23','AUTO');
