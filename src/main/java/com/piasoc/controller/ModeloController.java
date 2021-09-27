81dac0e307d5d0cd27a332233600e5fc247c1a9f
RdzSebastian <rdzsebastian@gmail.com>
2021-09-27 01:28:18 -0300

    Ajusta save de Modelo

--- src/main/java/com/piasoc/controller/ModeloController.java
+++ src/main/java/com/piasoc/controller/ModeloController.java
@@ -1,6 +1,9 @@
 package com.piasoc.controller;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.piasoc.commons.GeneralPath;
+import com.piasoc.model.Marca;
 import com.piasoc.model.Modelo;
 import com.piasoc.model.TipoVehiculo;
 import com.piasoc.service.ModeloService;
@@ -35,6 +39,18 @@ public class ModeloController {
 
 	@GetMapping("/saveModelo/{id}")
 	public String showSave(@PathVariable("id") Long id, Model model) {
+		List<Modelo> modelos = modeloService.getAll();
+
+		List<TipoVehiculo> tiposVehiculos = modelos.stream().map(Modelo::getTipoVehiculo).collect(Collectors.toList());
+		List<String> tiposVehiculosS = tiposVehiculos.stream().map(TipoVehiculo::getNombre).collect(Collectors.toList());
+		Set<String> tiposVehiculosSet = new HashSet<String>(tiposVehiculosS);
+		model.addAttribute("tipos", tiposVehiculos);
+		
+		List<Marca> marcas = modelos.stream().map(Modelo::getMarca).collect(Collectors.toList());
+		List<String> marcasS = marcas.stream().map(Marca::getNombre).collect(Collectors.toList());
+		Set<String> marcasSet = new HashSet<String>(marcasS);
+		model.addAttribute("marcas", marcas);
+		
 		if(id != null && id != 0) {
 			model.addAttribute("modelo", modeloService.get(id));
 		}else {
@@ -44,7 +60,7 @@ public class ModeloController {
 	}
 
 	@PostMapping("/saveModelo")
-	public String savE(Modelo modelo, Model model) {
+	public String save(Modelo modelo, Model model) {
 		modeloService.save(modelo);
 		return "redirect:/" +  "abmModelo";
 	}
