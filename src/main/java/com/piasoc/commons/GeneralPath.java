bbd6eb66b7ee5998639ebe938df68a55c94bacf2
RdzSebastian <rdzsebastian@gmail.com>
2022-04-01 15:12:58 -0300

    Refactor the common paths

--- src/main/java/com/piasoc/commons/GeneralPath.java
+++ src/main/java/com/piasoc/commons/GeneralPath.java
@@ -5,5 +5,15 @@ public class GeneralPath {
 	public static String TIPO_VEHICULO = "tipoVehiculo";
 	public static String MARCA = "marca";
 	public static String MODELO = "modelo";
+	
+	public static String ABM_TIPO_VEHICULO = "abmTipoVehiculo";
+	public static String ABM_MARCA = "abmMarca";
+	public static String ABM_MODELO = "abmModelo";
+
+	public static String SAVE_TIPO_VEHICULO = "saveTipoVehiculo";
+	public static String SAVE_MARCA = "saveMarca";
+	public static String SAVE_MODELO = "saveModelo";
+
+	public static String REDIRECT = "redirect:/";
 	public static String PATH_SEPARATOR = "/";
 }
