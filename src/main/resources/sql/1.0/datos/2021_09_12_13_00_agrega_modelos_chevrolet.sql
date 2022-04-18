1cc7fe6adc52f1f958a3b7ef791ea13e847dfd08
sebassalvador <sebastian.e.slvdr@gmail.com>
2022-04-18 19:05:11 -0300

    AGREGA BASE DE DATOS CHEVROLET

--- src/main/resources/sql/1.0/datos/2021_09_12_13_00_agrega_modelos_chevrolet.sql
+++ src/main/resources/sql/1.0/datos/2021_09_12_13_00_agrega_modelos_chevrolet.sql
@@ -20,8 +20,106 @@ begin
     marca_id := (SELECT id FROM MARCA WHERE nombre = marca_nombre);
 
     INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
-        VALUES ('A6',tipo_auto_id,marca_id,'1994', NULL);
-    
-   
+        VALUES ('CRUZE',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('BALENO',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('CORSA',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('ASTRA',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('BLAZER',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('VECTRA',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+       VALUES ('AGILE',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('AVEO',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('CAPTIVA',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('CLASSIC',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('S10',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('SPARK',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('ZAFIRA',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('CAMARO',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('COBALT',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('EQUINOX',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('MONTANA',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('ONIX',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('SPIN',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('PRISMA',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('TRACKER',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('TRAILBLAZER',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('CELTA',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('SPARK',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('SONIC',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('MERIVA',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('CORVETTE',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('AVALANCHE',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('GRAND VITARA',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('JIMNY',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('SILVERADO',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('LUV',tipo_auto_id,marca_id,'2010', NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('CORSA WAGON',tipo_auto_id,marca_id,'2010', NULL);
+        
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('SUBURBAN',tipo_auto_id,marca_id,'2010', NULL);
+
 
 end$$
