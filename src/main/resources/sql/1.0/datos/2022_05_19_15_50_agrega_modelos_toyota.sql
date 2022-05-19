09bfe2459c80878f6493f12d291f79df703359a8
Sebastian Salvador <Sebastian.e.slvdr@gmail.com>
2022-05-19 15:54:31 -0300

    AGREGA BASE DE DATOS TOYOTA

---
+++ src/main/resources/sql/1.0/datos/2022_05_19_15_50_agrega_modelos_toyota.sql
@@ -0,0 +1,130 @@
+do $$ 
+declare marca_nombre varchar := 'TOYOTA';
+        marca_id int := 0;
+        tipo_moto varchar := 'MOTO';
+        tipo_auto varchar := 'AUTO';
+        tipo_pickup varchar := 'PICKUP';
+        tipo_camion varchar := 'CAMION';
+        tipo_moto_id int := 0;
+        tipo_auto_id int := 0;
+        tipo_pickup_id int := 0;
+        tipo_camion_id int := 0;
+
+begin
+
+    tipo_moto_id :=(SELECT id FROM TIPO_VEHICULO WHERE nombre = tipo_moto);
+    tipo_auto_id := (SELECT id FROM TIPO_VEHICULO WHERE nombre = tipo_auto);
+    tipo_pickup_id := (SELECT id FROM TIPO_VEHICULO WHERE nombre = tipo_pickup);
+    tipo_camion_id := (SELECT id FROM TIPO_VEHICULO WHERE nombre = tipo_camion);
+
+    marca_id := (SELECT id FROM MARCA WHERE nombre = marca_nombre);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('86',tipo_auto_id,marca_id,'2012',NULL);
+
+     INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('AVENSIS',tipo_auto_id,marca_id,'2006','2010');
+
+     INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('CAMRY',tipo_auto_id,marca_id,'2000',NULL);
+
+     INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('COASTER',tipo_auto_id,marca_id,'2000','2000');
+
+     INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('COROLLA',tipo_auto_id,marca_id,'2000',NULL);
+
+     INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('CORONA',tipo_auto_id,marca_id,'2000','2003');
+
+     INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('ETIOS',tipo_auto_id,marca_id,'2013',NULL);
+
+     INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('HIACE',tipo_auto_id,marca_id,'2000',NULL);
+
+     INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('HILUX',tipo_pickup_id,marca_id,'2000',NULL);
+
+     INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('INNOVA',tipo_auto_id,marca_id,'2017',NULL);
+
+     INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('LAND CRUISER',tipo_pickup_id,marca_id,'2000',NULL);
+
+     INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('LEXUS',tipo_auto_id,marca_id,'2000','2006');
+
+     INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('PRIUS',tipo_auto_id,marca_id,'2009',NULL);
+
+     INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('RAV',tipo_pickup_id,marca_id,'2000',NULL);
+
+     INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('YARIS',tipo_auto_id,marca_id,'2016',NULL);
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+end$$
