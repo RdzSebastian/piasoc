11b3cf6cc523769be0dae82c013c8c8ed3ecc1d8
Sebastian Salvador <Sebastian.e.slvdr@gmail.com>
2022-06-29 21:26:16 -0300

    AGREGA BASE DE DATOS BAJAJ

---
+++ src/main/resources/sql/1.0/datos/2022_06_29_21_25_agrega_modelos_bajaj.sql
@@ -0,0 +1,57 @@
+do $$ 
+declare marca_nombre varchar := 'BAJAJ';
+        marca_id int := 0;
+        tipo_moto varchar := 'MOTO';
+        tipo_auto varchar := 'AUTO';
+        tipo_pickup varchar := 'PICKUP';
+        tipo_camion varchar := 'CAMION';
+        tipo_moto_id int := 0;
+        tipo_auto_id int := 0;
+        tipo_pickup_id int := 0;
+        tipo_camion_id int := 0;
+
+begin
+
+    tipo_moto_id :=(SELECT id FROM TIPO_VEHICULO WHERE nombre = tipo_moto);
+    tipo_auto_id := (SELECT id FROM TIPO_VEHICULO WHERE nombre = tipo_auto);
+    tipo_pickup_id := (SELECT id FROM TIPO_VEHICULO WHERE nombre = tipo_pickup);
+    tipo_camion_id := (SELECT id FROM TIPO_VEHICULO WHERE nombre = tipo_camion);
+
+    marca_id := (SELECT id FROM MARCA WHERE nombre = marca_nombre);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('AVENGER 220 CRUISE',tipo_moto_id,marca_id,'2013',NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('AVENGER 220 STREET',tipo_moto_id,marca_id,'2015',NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('DOMINAR 400',tipo_moto_id,marca_id,'2017',NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('ROUSER 160 NS',tipo_moto_id,marca_id,'2011',NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('ROUSER 180',tipo_moto_id,marca_id,'2011',NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('ROUSER 200 AS',tipo_moto_id,marca_id,'2017',NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('ROUSER 200 NS',tipo_moto_id,marca_id,'2011',NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('ROUSER 200 NS FI',tipo_moto_id,marca_id,'2018',NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('ROUSER 200 RS',tipo_moto_id,marca_id,'2017',NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('ROUSER 220 F',tipo_moto_id,marca_id,'2011',NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('V 15 150 VIKRANT',tipo_moto_id,marca_id,'2019',NULL);
+
+    
+      
+end$$
