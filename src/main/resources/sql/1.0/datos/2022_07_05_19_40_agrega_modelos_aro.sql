3fcf0e511ba28c49b1ba2ac89f8499ed389a33d6
Sebastian Salvador <Sebastian.e.slvdr@gmail.com>
2022-07-06 21:02:49 -0300

    AGRGA BASE DE DATOS ARO

---
+++ src/main/resources/sql/1.0/datos/2022_07_05_19_40_agrega_modelos_aro.sql
@@ -0,0 +1,36 @@
+do $$ 
+declare marca_nombre varchar := 'ARO';
+        marca_id int := 0;
+        tipo_moto varchar := 'MOTO';
+        tipo_auto varchar := 'AUTO';
+        tipo_pickup varchar := 'PICKUP';
+        tipo_camion varchar := 'CAMION';
+        tipo_moto_id int := 0;
+        tipo_auto_id int := 0;
+        tipo_pickup_id int := 0;
+        tipo_camion_id int := 0;
+
+begin
+
+    tipo_moto_id :=(SELECT id FROM TIPO_VEHICULO WHERE nombre = tipo_moto);
+    tipo_auto_id := (SELECT id FROM TIPO_VEHICULO WHERE nombre = tipo_auto);
+    tipo_pickup_id := (SELECT id FROM TIPO_VEHICULO WHERE nombre = tipo_pickup);
+    tipo_camion_id := (SELECT id FROM TIPO_VEHICULO WHERE nombre = tipo_camion);
+
+    marca_id := (SELECT id FROM MARCA WHERE nombre = marca_nombre);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('10.1',tipo_pickup_id,marca_id,'2000','2002');
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('10.4',tipo_pickup_id,marca_id,'2000','2002');
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('10.9',tipo_pickup_id,marca_id,'2000','2002');
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('SPARTANA',tipo_pickup_id,marca_id,'2000','2002');
+
+    
+
+end$$
