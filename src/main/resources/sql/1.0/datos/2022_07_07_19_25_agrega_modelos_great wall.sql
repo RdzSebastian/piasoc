ee4d53cf3228ec4424b8ce94a1fd9e1b0d255149
Sebastian Salvador <Sebastian.e.slvdr@gmail.com>
2022-07-07 21:42:23 -0300

    AGREGA BASE DE DATOS GREAT WALL

---
+++ src/main/resources/sql/1.0/datos/2022_07_07_19_25_agrega_modelos_great wall.sql
@@ -0,0 +1,31 @@
+do $$ 
+declare marca_nombre varchar := 'GREAT WALL';
+        marca_id int := 0;
+        tipo_moto varchar := 'MOTO';
+        tipo_auto varchar := 'AUTO';
+        tipo_pickup varchar := 'PICKUP';
+        tipo_camion varchar := 'CAMION';
+        tipo_moto_id int := 0;
+        tipo_auto_id int := 0;
+        tipo_pickup_id int := 0;
+        tipo_camion_id int := 0;
+
+begin
+
+    tipo_moto_id :=(SELECT id FROM TIPO_VEHICULO WHERE nombre = tipo_moto);
+    tipo_auto_id := (SELECT id FROM TIPO_VEHICULO WHERE nombre = tipo_auto);
+    tipo_pickup_id := (SELECT id FROM TIPO_VEHICULO WHERE nombre = tipo_pickup);
+    tipo_camion_id := (SELECT id FROM TIPO_VEHICULO WHERE nombre = tipo_camion);
+
+    marca_id := (SELECT id FROM MARCA WHERE nombre = marca_nombre);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('WINGLE 5',tipo_pickup_id,marca_id,'2019',NULL);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('WINGLE 6',tipo_pickup_id,marca_id,'2019',NULL);
+
+
+    
+      
+end$$
