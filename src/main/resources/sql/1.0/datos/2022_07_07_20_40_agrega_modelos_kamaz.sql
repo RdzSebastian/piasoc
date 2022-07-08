93983671ee22fcbe44dc72c40b69377095fca250
Sebastian Salvador <Sebastian.e.slvdr@gmail.com>
2022-07-07 21:44:37 -0300

    AGREGA BASE DE DATOS KAMAZ

---
+++ src/main/resources/sql/1.0/datos/2022_07_07_20_40_agrega_modelos_kamaz.sql
@@ -0,0 +1,44 @@
+do $$ 
+declare marca_nombre varchar := 'KAMAZ';
+        marca_id int := 0;
+        tipo_moto varchar := 'MOTO';
+        tipo_auto varchar := 'AUTO';
+        tipo_pickup varchar := 'PICKUP';
+        tipo_camion varchar := 'CAMION';
+        tipo_moto_id int := 0;
+        tipo_auto_id int := 0;
+        tipo_pickup_id int := 0;
+        tipo_camion_id int := 0;
+
+begin
+
+    tipo_moto_id :=(SELECT id FROM TIPO_VEHICULO WHERE nombre = tipo_moto);
+    tipo_auto_id := (SELECT id FROM TIPO_VEHICULO WHERE nombre = tipo_auto);
+    tipo_pickup_id := (SELECT id FROM TIPO_VEHICULO WHERE nombre = tipo_pickup);
+    tipo_camion_id := (SELECT id FROM TIPO_VEHICULO WHERE nombre = tipo_camion);
+
+    marca_id := (SELECT id FROM MARCA WHERE nombre = marca_nombre);
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('53212',tipo_camion_id,marca_id,'2000','2002');
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('53215',tipo_camion_id,marca_id,'2000','2002');
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('53229',tipo_camion_id,marca_id,'2000','2002');
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('54112',tipo_camion_id,marca_id,'2000','2002');
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('54115',tipo_camion_id,marca_id,'2002','2003');
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('55111',tipo_camion_id,marca_id,'2000','2002');
+
+    INSERT INTO MODELO (nombre, tipo_vehiculo_id, marca_id, anio_desde, anio_hasta)
+        VALUES ('65115',tipo_camion_id,marca_id,'2002','2003');
+
+     
+end$$
