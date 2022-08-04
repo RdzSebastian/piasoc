3c494a71bed5fbbfd51b79de671ae19cd65878f3
RdzSebastian <rdzsebastian@gmail.com>
2022-08-04 14:04:03 -0300

    Email refactor

---
+++ src/main/java/com/estonianport/piasoc/commons/emailService/Email.java
@@ -0,0 +1,13 @@
+package com.estonianport.piasoc.commons.emailService;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Email {
+	private String content;
+	private String email;
+	private String subject;
+
+}
--- src/main/java/com/estonianport/piasoc/commons/emailService/EmailService.java
+++ src/main/java/com/estonianport/piasoc/commons/emailService/EmailService.java
@@ -1,5 +1,5 @@
 
-package com.estonianport.piasoc.email;
+package com.estonianport.piasoc.commons.emailService;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -11,19 +11,21 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import com.estonianport.piasoc.commons.dateUtil.DateUtil;
+import com.estonianport.piasoc.model.Cliente;
 import com.estonianport.piasoc.model.Cotizacion;
+import com.estonianport.piasoc.model.DatosVehiculo;
 import com.estonianport.piasoc.model.Modelo;
 
 @Service
-public class EmailService implements EmailPort{
+public class EmailService{
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
 
 	@Autowired
 	private JavaMailSender sender;
 
-	@Override
-	public void sendEmail(EmailBody emailBody)  {
+	public void sendEmail(Email emailBody)  {
 		sendEmailTool(emailBody.getContent(), emailBody.getEmail(), emailBody.getSubject());
 	}
 
@@ -41,17 +43,18 @@ public class EmailService implements EmailPort{
 		}
 	}
 	
-	@Override
-	public void enviarMailComprabanteReserva(Cotizacion cotizacion) {
+	public void enviarMailComprabanteCliente(Cotizacion cotizacion) {
 
-		EmailBody emailBody = new EmailBody();
-		emailBody.setEmail("estonianport@gmail.com");
-		emailBody.setSubject("Solicitud de cotizacion de seguro");
+		Email emailBody = new Email();
 		
 		Modelo modelo = cotizacion.getDatosVehiculo().getModelo();
+		Cliente cliente = cotizacion.getCliente();
+		
+		emailBody.setEmail("estonianport@gmail.com");
+		emailBody.setSubject("Solicitud de cotizacion de seguro");
 		emailBody.setContent(
-				"Buenos dias " + cotizacion.getCliente().getApellido() + ", " + cotizacion.getCliente().getNombre() + "<br>" +
-				"Su " + modelo.getTipoVehiculo().getNombre() + ": " + modelo.getNombre() + ", " +  modelo.getMarca().getNombre() + " ha sido procesado exitosamente." + "<br>" +
+				"Buenos dias " + cliente.getApellido() + ", " + cliente.getNombre() + "<br>" +
+				"Su " + modelo.getTipoVehiculo().getNombre() + ": " + modelo.getMarca().getNombre()+ ", " + modelo.getNombre() + " ha sido procesado exitosamente." + "<br>" +
 				"Nuestros asesores procederan a enviarle la tasacion del seguro lo antes posible." + "<br>" +
 				"Muchisimas gracias por utilizar nuestros servicios.");
 
@@ -59,10 +62,52 @@ public class EmailService implements EmailPort{
 		this.sendEmail(emailBody);
 
 		//Envio a mail de la empresa
-		emailBody.setEmail("rdzsebastian@gmail.com");
+		emailBody.setEmail(cotizacion.getCliente().getEmail());
 		this.sendEmail(emailBody);
 
-		
+	}
+	
+	public void enviarMailComprabanteAseguradora(Cotizacion cotizacion) {
+
+		Email emailBody = new Email();
+
+		Modelo modelo = cotizacion.getDatosVehiculo().getModelo();
+		Cliente cliente = cotizacion.getCliente();
+		DatosVehiculo datosVehiculo = cotizacion.getDatosVehiculo();
+
+		emailBody.setEmail("estonianport@gmail.com");
+		emailBody.setSubject("Nueva consulta de " + cliente.getApellido() + ", " + cliente.getNombre());
+		emailBody.setContent(
+				"El cliente " + cliente.getApellido() + ", " + cliente.getNombre() + "<br>" +
+				"Quiere realizar una consulta de cotizacion" + "<br> <br>" +
+				"Vehiculo:" + "<br>" +
+				modelo.getTipoVehiculo().getNombre() + ": " + modelo.getMarca().getNombre()+ ", " + modelo.getNombre() + "<br>" +
+				"<br>" +
+				"Datos del Vehiculo:" + "<br>" +
+				"Intervalo de edad: " + datosVehiculo.getIntervaloEdad().getIntervalo() + "<br>" +
+				"Intervalo de Kilometros: " + datosVehiculo.getIntervaloKilometros().getIntervalo() + "<br>" +
+				"Tiene alarma: " + (datosVehiculo.isAlarma() ? "Si" : "No") + "<br>" +
+				"Se guarda en garage: " + (datosVehiculo.isGarage() ? "Si" : "No") + "<br>" +
+				"Es de uso particular: " + (datosVehiculo.isParticular() ? "Si" : "No") + "<br>" +
+				"Maneja el auto un menor de 25 años: " + (datosVehiculo.isHijos() ? "Si" : "No") + "<br>" +
+				"<br>" +
+				"Datos del cliente:" + "<br>" +
+				"Direccion: " + cliente.getCiudad() + ", " + cliente.getProvincia() + "<br>" +
+				"Codigo Postal: " + cliente.getCodigoPostal() + "<br>" +
+				"Cuil: " + cliente.getCuil() + "<br>" +
+				"Email: " + cliente.getEmail() + "<br>" +
+				"Fecha de nacimiento: " + DateUtil.getFecha(cliente.getFechaNacimiento()) + "<br>" +
+				"Sexo: " + cliente.getSexo().getNombre() + "<br>" +
+				"Empresa: " + (cliente.getEmpresa() != null && cliente.getEmpresa().isEmpty() ? cliente.getEmpresa() : "-") + "<br>" +
+				"Comentarios: " + (cliente.getComentarios() != null && cliente.getComentarios().isEmpty()  ? cliente.getComentarios() : "-") + "<br>");
+
+		// Envio al cliente
+		this.sendEmail(emailBody);
+
+		//Envio a mail de la empresa
+		emailBody.setEmail("estonianport@gmail.com");
+		this.sendEmail(emailBody);
+
 	}
 
 }
