c53e48d209d2f4924b4b4c65dfa39ff41fa96780
RdzSebastian <rdzsebastian@gmail.com>
2022-09-09 13:06:26 -0300

    Add CORS config for ajax request

---
+++ src/main/java/com/estonianport/piasoc/commons/mvcConfig/MvcConfig.java
@@ -0,0 +1,10 @@
+package com.estonianport.piasoc.commons.mvcConfig;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfig implements WebMvcConfigurer {
+
+	// Implementar esta clase WebMvcConfigurer es necesario para que las consultas AJAX
+	// sin Spring Security se puedan realizar a localhost
+
+}
